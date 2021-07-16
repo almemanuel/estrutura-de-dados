4e0170f1b80e8c86a75b7762e8d82a24ae096a7a
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-07-16 11:49:31 -0300

    On branch main 	new file:   string-cp.c

---
+++ 00-revisoes/01-prova/ponteiros/string-cp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void stringCopy(char *copy, char *paste) {
+    int i;
+    // a cópia será feita desconsiderando o caracter '\0'
+    for(i = 0; *(copy + i) != '\0'; i++) {
+        if(*(copy + i) != '\0') {
+            *(paste + i) = *(copy + i);
+        }
+    }
+}
+
+
+int main() {
+    char str1[] = "sex tape";
+    char str2[20];
+
+    stringCopy(str1, str2);
+    printf("%s\n", str2);
+
+    return 0
+}
