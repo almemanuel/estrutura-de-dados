8dfe00c22ec20517b6e6b67d3f51bc2304ad9237
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-07-18 16:59:49 -0300

    On branch main 	new file:   printList.c 	modified:   push.c

---
+++ 00-revisoes/01-prova/listas/printList.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct dataNode {
+    int id;
+} DataNode;
+
+typedef struct node {
+    DataNode data;
+    struct node* next;
+} Node;
+
+typedef struct List {
+    int size;
+    Node *head;
+} List;
+
+
+List *createList() {
+    List *list = (List *) malloc(sizeof(List));
+
+    list->size = 0;
+    list->head = NULL;
+
+    return list;
+}
+
+
+void push(List *list, DataNode data) {
+    Node *node = (Node *) malloc(sizeof(Node));
+
+    node->data = data;
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+}
+
+/* imprime a lista  */
+void printList(List *list) {
+    Node *pointer = list->head; /* ponteiro que aponta para a cabeça da listas  */
+
+    if(pointer == NULL) {       /* quando o ponteiro nao aponta para endereço algum significa que a lista chegou está vazia */
+        printf("Lista vazia");
+    }
+
+    while(pointer != NULL) {    /* ou, nesse caso, a posição já nao pertence a lista    */
+        printf("%i ", pointer->data.id);
+        pointer = pointer->next;
+    }
+}
+
+int main() {
+    List* l = createList();
+
+    DataNode data;
+    data.id = 5;
+
+    push(l, data);
+
+    data.id = 9;
+    push(l, data);
+
+    printList(l);
+
+    return 0;
+}
---
+++ 00-revisoes/01-prova/listas/push.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct dataNode {
+    int id;
+} DataNode;
+
+typedef struct node {
+    DataNode data;
+    struct node* next;
+} Node;
+
+typedef struct List {
+    int size;
+    Node *head;
+} List;
+
+
+List *createList() {
+    List *list = (List *) malloc(sizeof(List));
+
+    list->size = 0;
+    list->head = NULL;
+
+    return list;
+}
+
+/* cria um nó no começo */
+void push(List *list, DataNode data) {
+    Node *node = (Node *) malloc(sizeof(Node)); /* aloca o espaço para o nó */
+
+    node->data = data;          /* cria um dado para o nó               */
+    node->next = list->head;    /* torna o próximo nó o cabeça da lista */
+    list->head = node;          /* torna a cabeça da lista em nó        */
+    list->size++;               /* incrementa o tamanho da lista        */
+}
+
+int main() {
+    List* l = createList();
+
+    DataNode data;
+    data.id = 5;
+
+    push(l, data);
+
+    data.id = 9;
+    push(l, data);
+
+    printf("%i %i", l->head->data.id, l->head->next->data.id); /* imprime o primeiro e o segundo nó */
+    /* exit: 9 5    */
+
+    return 0;
+}
