a0784d7fd7858a4e4d2f90c7dbbbe95770a3a61f
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-07-21 23:42:58 -0300

    On branch main 	new file:   incluir.c 	new file:   leitura.c 	new file:   leitura2.c 	new file:   num.txt 	new file:   str.txt 	modified:   txt.c

--- 00-revisoes/01-prova/files/txt.c
+++ 00-revisoes/01-prova/files/txt.c
@@ -4,8 +4,8 @@ int main() {
     FILE *file;                     /* FILE é um tipo de variável */
     file = fopen("test.txt", "w");  /* fopen() faz que o ponteiro aponte para um arquivo dito no endereço, considerando o local atual como  */
                                     /* referência, e alguns atributos podem ser dados; nesse caso, "w" sobescreve                           */
-    fprintf(file, "C is suck");     /* fprintf() é uma função semelhante ao printf, que escreve no arquivo a mensagem inserida              */
-    fclose(file);                   /*  sala o arquivo                                                                                      */
+    fprintf(file, "C eh osso");     /* fprintf() é uma função semelhante ao printf, que escreve no arquivo a mensagem inserida              */
+    fclose(file);                   /*  salva o arquivo                                                                                     */
 
     return 0;
 }
