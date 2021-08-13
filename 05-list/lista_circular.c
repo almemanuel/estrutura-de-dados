76736c5565c20cf231ffbfa452fc12788af52007
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-08-13 16:00:56 -0300

    On branch main 	new file:   05-list/lista_circular.c

---
+++ 05-list/lista_circular.c
@@ -0,0 +1,161 @@
+/*
+Lista onde cada elemento aponta para o seu sucessor e o último elemento aponta para o primeiro
+Logo, não existe final na lista
+
+        inicio
+           |
+dados     |33| |-->|23| |-->|16| |--¬
+       |->|__|_|   |__|_|   |__|_|  |
+       |____________________________|
+
+- cada elemento é tratado como um ponteiro alocado dinamicamente a medida que os dados são inseridos
+- para o primeiro elemento, é utilizado um ponteiro de ponteiro
+
+Vantagens:
+- melhor utilização da memória
+- não precisa movimentar os elementos nas operações de inserção e remoção
+- possibilidade de percorrer a lista diversas vezes
+- não precisa-se considerar casos especiais de inclusão e remoção (primeiro e último)
+
+Desvantagens:
+- acesso indireto
+- necessidade de percorrer a lista para acessar um elemento
+- lista não possui final definido
+
+Usar quando:
+- não há necessidade de garantir um espaço mínimo para a execução do aplicativo
+- inserção/remoção em lista ordenada são as operações mais frequentes
+- quando há necessidade de voltar ao primeiro item da lista depois de percorrê-la
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/* STRUCTS */
+typedef struct dados {
+    int chave;
+} Dados;
+
+typedef struct no {
+    Dados dados;
+    struct no *prox;
+} No;
+
+typedef No *Lista;
+
+/* PROTÓTIPOS */
+Lista *cria_lista();
+void libera_lista(Lista *lista);
+int tamanho_lista(Lista *lista);
+int lista_vazia(Lista *lista);
+void insere_lista_inicio(Lista *lista, Dados dados);
+void insere_lista_final(Lista *lista, Dados dados);
+void insere_lista_ordenada(Lista *lista, Dados dados);/*substituir por funções que ordenem*/
+/* PROGRAMA PRINCIPAL */
+
+/* FUNÇÕES E PROCEDIMENTOS */
+Lista *cria_lista() {
+    Lista *lista = (Lista *) malloc(sizeof(Lista));
+    if(lista != NULL) *lista = NULL;
+    return lista;
+}
+
+
+void libera_lista(Lista *lista) {
+    if(lista != NULL && *lista != NULL) {
+        No *aux, *no = *lista;
+        while(*lista != no->prox) {
+            aux = no;
+            no = no->prox;
+            free(aux);
+            aux = NULL;
+        }
+        free(no);
+        no = NULL;
+        free(lista);
+        lista = NULL;
+    }
+}
+
+
+int tamanho_lista(Lista *lista) {
+    if(lista == NULL || *lista == NULL) return -1;
+    int cont = 0;
+    No *no = *lista;
+    do {
+        cont++;
+        no = no->prox;
+    } while(no != *lista);
+    return cont;
+}
+
+
+int lista_vazia(Lista *lista) {
+    if(lista == NULL && *lista == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+
+void insere_lista_inicio(Lista *lista, Dados dados) {
+    if(lista == NULL) return;
+    No *no = (No *) malloc(sizeof(No));
+    if(no == NULL) return;
+    no->dados = dados;
+    if(*lista == NULL) {
+        *lista = no;
+        no->prox = no;
+    } else {
+        No *aux = *lista;
+        while(aux->prox != *lista)
+            aux = aux->prox;
+        aux->prox = no;
+        no->prox = *lista;
+        *lista = no;
+    }
+}
+
+
+void insere_lista_final(Lista *lista, Dados dados) {
+    if(lista == NULL) return;
+    No *no = (No *) malloc(sizeof(No));
+    if(no == NULL) return;
+    no->dados = dados;
+    if(*lista == NULL) {
+        insere_lista_inicio(lista, dados);
+    } else {
+        No *aux = *lista;
+        while(aux->prox != *lista)
+            aux = aux->prox;
+        aux->prox = no;
+        no->prox = *lista;
+    }
+}
+
+
+void insere_lista_ordenada(Lista *lista, Dados dados) {
+    if(lista == NULL) return;
+    No *no = (No *) malloc(sizeof(No));
+    if(no == NULL)
+        return 0;
+    no->dados = dados;
+    if(*lista == NULL) insere_lista_inicio(lista, dados);
+    else if((*lista)->dados.chave > dados.chave) {
+        No *atual = *lista;
+        while(atual->prox != *lista) {
+            atual = atual->prox;
+            no->prox = *lista;
+            atual->prox = no;
+            *lista = no;
+            return;
+        }
+    }
+    No *ant = *lista, *atual = (*lista)->prox;
+    while(atual != *lista && atual->dados.chave < dados.chave) {
+        ant = atual;
+        atual = atual->prox;
+    }
+    ant->prox = no;
+    no->prox = atual;
+}
