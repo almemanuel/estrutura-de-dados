6acbf5616ba6f169de431feda12ae2470da3f527
Emanuel Almeida <emanuel.almeida1998@outlook.com>
2021-08-27 15:29:56 -0300

    fgetc

--- arquivos/code.c
+++ arquivos/code.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
+/* Exemplo escrita de caractere
 int main() {
     FILE *f = fopen("arquivo.txt", "w");
     if(f == NULL) {
@@ -11,11 +12,26 @@ int main() {
     char txt[20] = "My program at C";
     int i;
 
-    /* escreverá um caractere a cada iteração */
     for(i = 0; i < strlen(txt); i++)
         fputc(txt[i], f);
         fputc('*', f);
     fclose(f);
 
+    return 0;
+} */
+
+int main() {
+    FILE *f = fopen("arquivo.txt", "r");
+    if(f == NULL) {
+        printf("ERRO NA ABERTURA");
+        exit(1);
+    }
+    char c;
+    int i;
+    for(i = 1; i <= 10; i++) {
+        c = fgetc(f);
+        printf("%c", c);
+    }
+    fclose(f);
     return 0;
 }
