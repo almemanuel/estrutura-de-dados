beaf9c42de88ee04c147db6250c500a10784a899
Emanuel Almeida <emanuel.almeida1998@outlook.com>
2021-09-24 19:53:41 -0300

    str

--- 01-revisao-strings/comprimento-str.c
+++ 01-revisao-strings/comprimento-str.c
@@ -4,17 +4,24 @@ Crie um programa que calcula o comprimento de uma string (nao use a função s
 
 #include<stdio.h>
 
+int string_len(char str[50]);
+
 int main() {
     char string[50];
 
     printf("Digite qualquer coisa: ");
     fgets(string, 50, stdin);
 
-    int i, cont = 0;
-    for(i = 0; string[i] != '\n'; i++) {
+    int len = string_len(string);
+
+    printf("A string digitada eh um vetor com %i posicoes\n", len);
+    return 0;
+}
+
+int string_len(char str[50]) {
+    int i, cont;
+    for(i = 0; str[i] != '\0'; i++) {
         cont++;
     }
-
-    printf("A string digitada eh um vetor com %i posicoes", cont);
-    return 0;
+    return cont - 1;
 }
